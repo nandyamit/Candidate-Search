df815c5ca119717b2cbc82d36160c36a36da2507
nandyamit <nandyamit.in@gmail.com>
2024-11-09 16:58:41 -0800

    Added _redirect file and updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-// vite.config.ts
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_GITHUB_TOKEN': JSON.stringify(env.VITE_GITHUB_TOKEN)
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
